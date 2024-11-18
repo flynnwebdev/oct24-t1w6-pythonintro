46e8aaf8af82134e4741121de36286e90c01918b
Mr. Robot <flynnwebdev@gmail.com>
2024-11-18 19:59:03 +1000

    Lesson done

---
+++ main.py
@@ -0,0 +1,87 @@
+# # print(2 + 3)
+
+# # # The below line prints Hello World
+# # # Python interprer will ignore comments
+# # print('Hello World!!')
+
+# # print('Goodbye!') # See ya!
+
+
+# # Data Types
+
+# # Numbers
+
+# # int
+# # 5
+# # 1000
+# print(type(5))
+
+# # float
+# # 2.5
+# # 5.3
+# # 3.14159
+# print(type(3.14159))
+
+# # complex
+# # 2 + 3j
+# print(type(2 + 3j))
+
+# # Number Operations
+# print(2 + 3)
+# print(2 - 3)
+# print(5 * 2) # Multiplication
+# print(10 / 5) # Float division
+# print(int(10 // 3.5)) # Integer division (with typecast)
+# print(5 % 2.25) # Modulo (remainder)
+
+# # Strings
+# # "Hello World!!"
+# # 'Hello World!!'
+# print('It\'s Monday!')
+# print(type('Hello'))
+
+# # Concatenation
+# print('Hello ' + 'World!')
+
+# # String methods
+# print('Hello'.lower())
+# print('Hello World'.replace('World', 'There'))
+
+# # Boolean
+# # True
+# # False
+# print(type(True))
+
+# # Falsy values
+# print(bool(0))
+# print(bool(''))
+# print(bool(None))
+# print(bool([]))
+# print(bool(()))
+# print(bool({}))
+
+# Comparison Operators
+# <, >, <=, >=, ==
+# print(2 < 3)
+# print(2 > 3)
+# print(5 >= 5)
+# print(5 == 5)
+
+# Logical Operators
+# and, or, not
+print(True and False)
+print(False or True)
+print(5 > 3 and 2 < 5)
+
+# Variables
+num1 = 5
+num2 = 4
+print(num1 + num2)
+num2 = 10
+print(num1 + num2)
+
+# String interpolation
+name = "Matt"
+print('My name is ' + name + " and I live in Brisbane")
+# f-string
+print(f'My name is {name} and I live in Brisbane')
